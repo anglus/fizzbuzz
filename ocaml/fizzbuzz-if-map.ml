dcb12dacc52e16e3c86d107607b9e44c00e1c42c
Matthew Morris <anglus@gmail.com>
2017-01-12 21:59:22 -0600

    Add OCaml examples.

---
+++ ocaml/fizzbuzz-if-map.ml
@@ -0,0 +1,26 @@
+#!/usr/bin/env ocaml
+
+(* Copyright (c) 2017, Matthew Morris
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. *)
+
+let fizzbuzz num = if num mod 3 == 0 && num mod 5 == 0 then "fizzbuzz" else
+                   if num mod 3 == 0 then "fizz" else
+                   if num mod 5 == 0 then "buzz" else
+                   string_of_int num;;
+
+let rec range i j = if i > j then [] else i :: (range (i + 1) j);;
+
+let numlist = range 1 100;;
+print_string (String.concat "\n" (List.map fizzbuzz numlist));;
+print_string "\n"
