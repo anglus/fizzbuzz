1400b838634c94d757075ed271106b7604175443
Matthew Morris <anglus@gmail.com>
2017-04-22 21:45:28 -0500

    Add Python examples using a dictionary.

---
+++ python/fizzbuzz-dict-fun.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Copyright (c) 2017, Matthew Morris
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""
+
+def fizzbuzz(n):
+    return {
+        (True, True)  : "fizzbuzz",
+        (True, False) : "fizz",
+        (False, True) : "buzz",
+    }.get((n % 3 == 0, n % 5 == 0), n)
+
+for num in range(1,101):
+    print(fizzbuzz(num))
