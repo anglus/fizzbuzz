88ae43e46036758921e27109df94893fd0851541
Matthew Morris <anglus@gmail.com>
2017-04-24 21:05:20 -0500

    Add Elixir examples.

---
+++ elixir/fizz_buzz_case.ex
@@ -0,0 +1,28 @@
+# Copyright (c) 2017, Matthew Morris
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+defmodule FizzBuzzCase do
+  defp fizzbuzz(n) do
+    case {(rem n, 3), (rem n, 5)} do
+      {0, 0} -> "fizzbuzz"
+      {0, _} -> "fizz"
+      {_, 0} -> "buzz"
+      _ -> n
+    end
+  end
+
+  def main do
+    for num <- 1..100, do: IO.puts fizzbuzz num
+  end
+end
---
+++ elixir/fizz_buzz_cond.ex
@@ -0,0 +1,28 @@
+# Copyright (c) 2017, Matthew Morris
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+defmodule FizzBuzzCond do
+  defp fizzbuzz(n) do
+    cond do
+      (rem n, 3) == 0 and (rem n, 5) == 0 -> "fizzbuzz"
+      (rem n, 3) == 0 -> "fizz"
+      (rem n, 5) == 0 -> "buzz"
+      true -> n
+    end
+  end
+
+  def main do
+    for num <- 1..100, do: IO.puts fizzbuzz num
+  end
+end
