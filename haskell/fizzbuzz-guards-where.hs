c919df2175e993d2767e63785447ee3888102448
Matthew Morris <anglus@gmail.com>
2017-04-24 22:43:00 -0500

    Add Haskell examples.

---
+++ haskell/fizzbuzz-guards-where.hs
@@ -0,0 +1,25 @@
+-- Copyright (c) 2017, Matthew Morris
+--
+-- Permission to use, copy, modify, and/or distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+fizzbuzz :: (Int) => String
+fizzbuzz n
+    | mod3 && mod5 = "fizzbuzz"
+    | mod3         = "fizz"
+    | mod5         = "buzz"
+    | otherwise    = show n
+    where mod3 = rem n 3 == 0
+          mod5 = rem n 5 == 0
+
+main = do
+    mapM putStrLn $ map fizzbuzz [1..100]
